6c33ab6205774449c49eecc0f72da0db74f9e830
jeonheeji <junheeji@gmail.com>
2025-09-26 11:14:41 +0900

    game만들기

--- battle/battle_manager.py
+++ battle/battle_manager.py
@@ -7,11 +7,13 @@ class BattleManager:
         self.character1 = character1
         self.character2 = character2
 
+    # 인스턴스 메서드 : start_battle 구현
     def start_battle(self):
-        print("⚔️ 전투 시작!")
+        print("----- 전투 시작! -----")
         time.sleep(1)
 
         # 선공자 랜덤 결정
+        # 공격자와 수비자를 입력받아서 결정
         if random.random() < 0.5:
             attacker, defender = self.character1, self.character2
         else:
@@ -21,6 +23,8 @@ class BattleManager:
         time.sleep(1)
 
         # 전투 루프
+        # 공격자 and 방어자가 살아있는동안 계속진행됨
+        # 방어자가 살아있다면
         while attacker.is_alive() and defender.is_alive():
             self.perform_turn(attacker, defender)
             time.sleep(1.5)
@@ -39,6 +43,8 @@ class BattleManager:
         winner = attacker if attacker.is_alive() else defender
         print(f"🏆 {winner.get_name()}의 승리!")
 
+    # 기본공격 vs 특수공격 선택:
+    # 랜덤 모듈을 통해 70% 확률로 기본공격, 30% 확률로 특수공격
     def perform_turn(self, attacker, defender):
         try:
             if random.random() < 0.7:
@@ -48,5 +54,6 @@ class BattleManager:
             else:
                 # 특수 공격
                 attacker.special_attack(defender)
+        # 예외발생 : 마법사 마나 부족시 공격 불가
         except Exception as e:
             print(f"⚠️ {attacker.get_name()}의 특수 공격 실패: {str(e)}")
--- characters/character.py
+++ characters/character.py
@@ -7,29 +7,36 @@ class Character(ABC):
         self.max_hp = max_hp
         self.hp = max_hp
 
+    # 기본공격 (추상메서드)
     @abstractmethod
     def attack(self):
         pass
 
+    # 특수공격 (추상메서드)
     @abstractmethod
     def special_attack(self, target):
         pass
 
+    # 피해를 입으면 체력이 감소
     def take_damage(self, amount):
         self.hp -= amount
         if self.hp < 0:
             self.hp = 0
         print(f"{self.name}이 {amount}데미지지를 입었다. 현재 체력은 {self.hp}")
 
+    # 체력이 0 이하이면 false 반환
     def is_alive(self):
         return self.hp > 0
 
+    # 캐릭터의 정보를 출력
     def show_status(self):
         print(f"이름:{self.name} : 체력 {self.hp}/{self.max_hp}")
 
+    # 캐릭터의 체력을 초기화
     def reset_health(self):
         self.hp = self.max_hp
-        print(f"reset {self.name}의 체력이 {self.max_hp}로 돌아왔습니다")
+        print(f"{self.name}의 체력이 {self.max_hp}로 돌아왔습니다")
 
+    # 캐릭터의 이름을 가져옴
     def get_name(self):
         return self.name
